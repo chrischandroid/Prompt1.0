983e27388ad32834267853054edb746b80011882
chris <cdinsh@163.com>
2024-12-08 23:01:14 +0800

    update login ui

--- app/src/main/java/com/taichi/prompts/android/activity/login/PhoneActivity.kt
+++ app/src/main/java/com/taichi/prompts/android/activity/login/PhoneActivity.kt
@@ -1,11 +1,15 @@
 package com.taichi.prompts.android.activity.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.taichi.prompts.android.R
+import com.taichi.prompts.android.activity.home.TabActivity
 
 class PhoneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,15 @@ class PhoneActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val layout: RelativeLayout = findViewById(R.id.button)
+        layout.setOnClickListener {
+            val editText: EditText = findViewById(R.id.place)
+            val inputText: String? = editText.text.toString()
+            if (inputText != null && inputText.equals("18000000000")) {
+                val intent = Intent(this, TabActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
