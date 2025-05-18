0ef6328f110188786f3189daa1b53a5ed492ec1d
chris <cdinsh@163.com>
2025-05-18 20:39:51 +0800

    update fragment common

--- app/src/main/java/com/taichi/prompts/android/fragment/common/FragCommon.kt
+++ app/src/main/java/com/taichi/prompts/android/fragment/common/FragCommon.kt
@@ -1,6 +1,8 @@
 package com.taichi.prompts.android.fragment.common
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import androidx.core.content.ContextCompat
 import com.taichi.prompts.android.BR
 import com.taichi.prompts.android.R
 import com.taichi.prompts.android.databinding.FragmentCommonBinding
@@ -23,11 +25,34 @@ class FragCommon : BaseFragment<FragmentCommonBinding, CommonListViewModel>() {
     }
 
 
+    @SuppressLint("ResourceAsColor")
     override fun initViewData() {
+        binding?.receive?.setOnClickListener {
+            setTabSelection(true)
+        }
 
+        binding?.send?.setOnClickListener {
+            setTabSelection(false)
+        }
+
+        // 默认选择"我收到的"
+        setTabSelection(true)
     }
 
+    private fun setTabSelection(isReceiveTab: Boolean) {
+        // 获取颜色资源
+        val blackColor = ContextCompat.getColor(requireContext(), R.color.black)
+        val grayColor = ContextCompat.getColor(requireContext(), R.color.darkgray)
 
-    private fun initListView() {
+        binding?.receive?.setTextColor(if (isReceiveTab) blackColor else grayColor)
+        binding?.send?.setTextColor(if (isReceiveTab) grayColor else blackColor)
+
+        // 更新提示文本
+        binding?.noseed?.text = if (isReceiveTab) {
+            "暂无种子\n先去别处瞅瞅吧"
+        } else {
+            "暂无已经发出的种子\n先看看推荐吧"
+        }
     }
+
 }
